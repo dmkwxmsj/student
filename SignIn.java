fc35d5bb46bbb082131a0fda64f0ffca0d2c4d08
mk <mk@qq.com>
2020-11-30 17:17:06 +0800

    在家开发签到功能其它部分

---
+++ SignIn.java
@@ -0,0 +1,17 @@
+/*
+* @Author: Marte
+* @Date:   2020-11-30 16:59:45
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-11-30 17:08:34
+*/
+
+public class java {
+    public static void main(String[] args) {
+        private int id
+        private String name
+        public void sign(){
+
+        }
+
+    }
+}
