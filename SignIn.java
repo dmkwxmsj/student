842f13583d81831019e83935d0c48f7a7a229f17
dmk <dmk@qq.com>
2020-11-30 17:15:51 +0800

    在公司开发继续签到功能

---
+++ SignIn.java
@@ -0,0 +1,14 @@
+/*
+* @Author: Marte
+* @Date:   2020-11-30 16:50:25
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-11-30 17:14:24
+*/
+
+public class SigIn {
+   private int id;
+   private String name;
+   public void sign(){
+    System.out.println("id - "+id);
+   }
+}
